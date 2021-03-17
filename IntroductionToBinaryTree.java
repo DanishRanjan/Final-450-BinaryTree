f4bbc6a80f347c9dae58c5d4ded9295374a9dffd
Danish Ranjan <ranjandanish@gmail.com>
2021-03-17 15:52:04 +0530

    Introduction to Binary Tree

---
+++ IntroductionToBinaryTree.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class IntroductionToBinaryTree {
+	public static class Node{
+		int data;
+		Node left;
+		Node right;
+		
+		Node(int data, Node left, Node right){
+			this.data = data;
+			this.left = left;
+			this.right = right;
+		}
+		
+	}
+	
+	public static void main(String args[]) throws Exception {
+		
+	}
+}
