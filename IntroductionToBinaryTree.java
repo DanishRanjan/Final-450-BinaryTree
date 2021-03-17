2b9bd94c6ca9db42a9b21767fc49da4e637fba32
Danish Ranjan <ranjandanish@gmail.com>
2021-03-17 16:58:43 +0530

    Generation Of Binary Tree

--- IntroductionToBinaryTree.java
+++ IntroductionToBinaryTree.java
@@ -1,19 +1,75 @@
 import java.util.*;
+
 public class IntroductionToBinaryTree {
-	public static class Node{
+	public static class Node {
 		int data;
 		Node left;
 		Node right;
-		
-		Node(int data, Node left, Node right){
+
+		Node(int data, Node left, Node right) {
 			this.data = data;
 			this.left = left;
 			this.right = right;
 		}
-		
+
 	}
-	
+
+	public static class Pair {
+		Node node;
+		int state;
+
+		Pair(Node node, int state) {
+			this.node = node;
+			this.state = state;
+		}
+	}
+
+//	1 is for left
+//	2 is for right
+//	3 is for pop
 	public static void main(String args[]) throws Exception {
+		Integer[] arr = { 50, 25, 12, null, null, 37, 30, null, null, null, 75, 62, null, 70, null, null, 87, null,
+				null };
+		Node root = new Node(arr[0], null, null);
+		Pair rootPair = new Pair(root, 1); 
+		Stack<Pair> st = new Stack<>();
+		st.push(rootPair);
 		
+		int index = 0;
+		
+		while (st.size() > 0) {
+			Pair top = st.peek();
+			if (top.state == 1) {
+				index++;
+				if (arr[index] != null) {
+					Node ln = new Node(arr[index], null, null);// left node
+					top.node.left = ln; // top k node k left mein abhi to new node banaya h usko point kar do
+
+					Pair lp = new Pair(ln, 1);// left pair
+					st.push(lp);
+
+				} else {
+					top.node.left = null;
+				}
+				top.state++; // left work is over
+			} else if (top.state == 2) {
+				index++;
+				if (arr[index] != null) {
+
+					Node rn = new Node(arr[index], null, null);// right node
+					top.node.right = rn; // top k node k left mein abhi to new node banaya h usko point kar do
+
+					Pair rp = new Pair(rn, 1);// right pair
+					st.push(rp);
+
+				} else {
+					top.node.right = null;
+				}
+				top.state++; // right work is over
+			} else {
+				st.pop();
+			}
+		}
+
 	}
 }
